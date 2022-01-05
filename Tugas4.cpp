2d7b21af5e1bb53651d48580905344bfdaf5916a
arifrhm <96586737+arifrhm@users.noreply.github.com>
2022-01-05 23:08:13 +0700

    Add files via upload

---
+++ Tugas4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int x;
+	x=30;
+	cout<<"Nilai Awal X : "<<x<<endl;
+	x--;
+	cout<<"Nilai Kedua X : "<<x<<endl;
+	x++;
+	x++;
+	cout<<"Nilai Ketiga X : "<<x<<endl;
+	return 0;
+}
